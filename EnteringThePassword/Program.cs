e5bc1e316dd9f5541c63958508ed6a40f9f286cd
User <User@DESKTOP-HSEQPU2>
2024-02-12 12:48:48 +0400

    Добавьте файлы проекта.

---
+++ EnteringThePassword/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnteringThePassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int triesCount = 3;
+            string password = "14638a@7";
+            string userInput;
+
+            for (int i = 1; i <= triesCount; i++)
+            {
+                Console.WriteLine("ВВедите пароль: ");
+                userInput = Console.ReadLine();
+                if (userInput == password)
+                {
+                    Console.WriteLine("Добро пожаловать!");
+                    break;
+                }
+                else Console.WriteLine($"Вы ввели неверный пароль. У вас осталось попыток: {triesCount - i}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
